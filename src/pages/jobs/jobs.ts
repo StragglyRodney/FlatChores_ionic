d91bece57f306f862066f98f506da6ac720a2808
Daniel Walker <daniel123.dw@gmail.com>
2018-08-29 13:53:05 +1200

    .

--- src/pages/jobs/jobs.ts
+++ src/pages/jobs/jobs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController,  NavParams, Nav, ToastController} from 'ionic-angular';
+import { NavController, NavParams, Nav, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { CreateJobPage } from '../create-job/create-job';
@@ -10,52 +10,52 @@ import { elementAt } from 'rxjs/operators';
   templateUrl: 'jobs.html'
 })
 export class Chores {
-  
+
   information: any[];
-  newJob="";
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController, public navParams: NavParams, private http: Http,private storage: Storage) {
-     let localata = this.http.get('assets/information.json').map(res => res.json().items);
-     localata.subscribe(data => {
-       this.information = data;
-     });
+  newJob = "";
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController, public navParams: NavParams, private http: Http, private storage: Storage) {
+    let localata = this.http.get('assets/information.json').map(res => res.json().items);
+    localata.subscribe(data => {
+      this.information = data;
+    });
   }
 
-  toggleSection(i){
+  toggleSection(i) {
     this.information[i].open = !this.information[i].open;
   }
 
-  toggleItem(i, j){
+  toggleItem(i, j) {
     this.information[i].children[j].open = !this.information[i].children[j].open;
 
   }
 
-   createJob(){
+  createJob() {
     this.navCtrl.push(CreateJobPage, {
-    callback: this.myCallbackFunction
-    },{animate: true, animation: "transition"});
+      callback: this.myCallbackFunction
+    }, { animate: true, animation: "transition" });
 
-   }
+  }
 
-   myCallbackFunction = (_params) => {
+  myCallbackFunction = (_params) => {
     return new Promise((resolve, reject) => {
-        this.newJob = _params;
-        this.addJob();
-        resolve();
+      this.newJob = _params;
+      this.addJob();
+      resolve();
     });
-   }
-   
-   addJob(){
-     //check if title of job already exists
-     console.log(this.information);
-     this.information.forEach(element => {
-      if(element['name'] == this.newJob[0]){
+  }
+
+  addJob() {
+    //check if title of job already exists
+    console.log(this.information);
+    this.information.forEach(element => {
+      if (element['name'] == this.newJob[0]) {
         this.showToast("Job already exisits");
       }
-     })
+    })
 
-   }
+  }
 
-   showToast(message) {
+  showToast(message) {
     let toast = this.toastCtrl.create({
       message: message,
       duration: 3000
